949453c7382eb930d250b2574c4f50cf9796f172
Fathers-gg <fullclip1941@gmail.com>
2024-12-16 18:17:03 +0500

    add_change_2

--- date_changer.py
+++ date_changer.py
@@ -1,4 +1,22 @@
-created_date = datetime.now().strftime("%d-%m-%Y-%A")#дата создания заметки
-print("дата создания заметки ", created_date)
-issue_date = datetime.now().strftime("%d-%m-%Y-%A")#дата дедлайна
-print("дата дедлайна", issue_date)
+from datetime import datetime
+while True:
+    temp = input('Введите дату в формате "дд-мм-гггг" ')
+    try:
+        form_date = datetime.strptime(temp, "%d-%m-%Y").date() #дата создания заметки
+    except ValueError:
+        print("Некорректный формат даты. Попробуйте ещё раз.")
+    else:
+        # Преобразуем дату в формат "дд-мм"
+        created_date = form_date.strftime("%d-%m")
+        print("Дата создания заметки:", created_date)
+        break
+
+temp= input('Введите дату в формате "дд-мм-гггг" ')
+try:
+    form_date = datetime.strptime(temp, "%d-%m-%Y").date()  #дата истечения заметки
+except ValueError:
+    print("Некорректный формат даты. Попробуйте ещё раз.")
+else:
+    # Преобразуем дату в формат "дд-мм"
+    issue_date = form_date.strftime("%d-%m")
+    print("Дата истечения заметки:", issue_date)
--- final.py
+++ final.py
@@ -10,18 +10,48 @@ title_2 = input() #заголовок заметки
 print("Введите заголовок заметки 3")
 title_3 = input() #заголовок заметки
 titles=[title_1, title_2, title_3]
-print(titles)
+
 print("Введите описание заметки")
 content = input() #Описание заметки
 
 print("введите статус заметки, например 'активна'") #статус заметки
 status = input()
 
-created_date = datetime.now().strftime("%d-%m-%Y-%A")#дата создания заметки
-print("дата создания заметки ", created_date)
-issue_date = datetime.now().strftime("%d-%m-%Y-%A")#дата дедлайна
-print("дата дедлайна", issue_date)
 
-information_about_note=[username, content, status, created_date, issue_date,  titles,]
-for item in information_about_note:
-    print(item)
+
+while True:
+    temp = input('Введите дату в формате "дд-мм-гггг" ')
+    try:
+        form_date = datetime.strptime(temp, "%d-%m-%Y").date() #дата создания заметки
+    except ValueError:
+        print("Некорректный формат даты. Попробуйте ещё раз.")
+    else:
+        # Преобразуем дату в формат "дд-мм"
+        created_date = form_date.strftime("%d-%m")
+        print("Дата создания заметки:", created_date)
+        break
+
+temp= input('Введите дату в формате "дд-мм-гггг" ')
+try:
+    form_date = datetime.strptime(temp, "%d-%m-%Y").date()  #дата истечения заметки
+except ValueError:
+    print("Некорректный формат даты. Попробуйте ещё раз.")
+else:
+    # Преобразуем дату в формат "дд-мм"
+    issue_date = form_date.strftime("%d-%m")
+    print("Дата истечения заметки:", issue_date)
+
+
+information_about_note={
+    "username": username,
+    "title_1": title_1,
+    "title_2": title_2,
+    "title_3": title_3,
+    "content": content,
+    "status": status,
+    "created_date": created_date,
+    "issue_date": issue_date
+}
+
+for key, value in information_about_note.items():
+    print(key + ": " + str(value))
